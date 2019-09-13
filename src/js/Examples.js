bc5ea5d7d3fb9c900c99408f349e290c0da5729f
wpower12 <willpowe@gmail.com>
2019-09-13 14:46:21 -0400

    cleanup.

--- src/js/Examples.js
+++ src/js/Examples.js
@@ -19,18 +19,18 @@ export let Add42 = [
 export let CountSub2 = [ 
 	0b00101111, // [00] ADD [15] - Load from memory at 15 (2) into B, put A+B in A
 	0b11100000, // [01] OUT      - Put A in OR, and Display
-	0b01110100, // [02] JC  [4]  - If Carry, Jump to 4 (Start of sub loop)
+	0b01110100, // [02] JC  [4]  - If Carry, Jump to 4 (Just before start of sub loop)
 	0b01100000, // [03] J   [0]  - Else,     Jump to 0 (Start of add loop)
-	0b00111111, // [04] SUB [15] - Load from memory at 15 (2) into B, put A-B in A
-	0b11100000, // [05] OUT      - Put A in OR, and Display
-	0b10000000, // [06]	JZ  [0]  - If Zero,  Jump to 0 (Start of add loop)
-	0b01100100, // [07] J   [4]  - Else,     Jump to 4 (Start of sub loop)
-	0b00000000, // [08]
+	0b00011110, // [04] LDA [14] - Load in 256.
+	0b00111111, // [05] SUB [15] - Load from memory at 15 (2) into B, put A-B in A
+	0b11100000, // [06] OUT      - Put A in OR, and Display
+	0b10000000, // [07]	JZ  [0]  - If Zero,  Jump to 0 (Start of add loop)
+	0b01100101, // [08] J   [5]  - Else,     Jump to 4 (Start of sub loop)
 	0b00000000, // [09]
 	0b00000000, // [10]			 	 
 	0b00000000, // [11]
 	0b00000000, // [12]
 	0b00000000, // [13]
-	0b00000000, // [14]
-	0b00000010];// [15] 2 - Value that's added/subbed. 
+	0b00000000, // [14] 256 - Max value
+	0b00000010];// [15] 2   - Value that's added/subbed. 
 
